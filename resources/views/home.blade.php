26c8f56b4b16836569a6e284379e8b42561e1adb
aleprosli <aliff.rosli96@gmail.com>
2023-10-10 16:50:36 +0800

    add card for senarai agensi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,42 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">Paparan Utama</div>
 
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                            {{ session( 'status') }}
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    Tahniah, anda telah login
+                </div>
+            </div>
+
+            <div class="card mt-2">
+                <div class="card-header">Senarai Agensi</div>
+                <div class="card-body">
+                    <button type="button" class="btn btn-secondary">Tambah Agensi</button>
+
+                    <table class="table">
+                        <thead>
+                          <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nama Agensi</th>
+                            <th scope="col">Tarikh Daftar</th>
+                            <th scope="col">Lain Lain</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                          </tr>
+                        </tbody>
+                      </table>
                 </div>
             </div>
         </div>
